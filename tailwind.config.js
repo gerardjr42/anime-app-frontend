e3d84f88ebee99002e6218aa4eaacb1ca4605fa5
gerardjr42 <ggarciajr12@gmail.com>
2024-07-28 09:32:10 -0400

    install npm packages and vercel config file

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'custom': ['GTSuper'],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+};
